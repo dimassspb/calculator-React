6393ba2794bb27e922d25f22658918c8df4b5018
Dmitrii Isakov <dimass82@mail.ru>
2021-09-08 15:17:30 +0300

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [result, setResult] = useState('');
+
+  const operators = ['/', '*', '+', '-', '.'];
+
+  const updateResult = (value) => {
+    if (
+      (operators.includes(value) && result === '') ||
+      (operators.includes(value) && operators.includes(result.slice(-1)))
+    ) {
+      return;
+    }
+    setResult(result + value);
+  };
+
+  const createDigits = (a, b) => {
+    const digits = [];
+    for (let i = a; i <= b; i++) {
+      digits.push(
+        <div onClick={() => updateResult(i.toString())} key={i}>
+          {i}
+        </div>,
+      );
+    }
+    return digits;
+  };
+
+  const Resultulate = () => {
+    setResult(eval(result).toString());
+  };
+
+  const reset = () => {
+    setResult('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Resultulator">
+      <div className="input">{result ? result : '0'}</div>
+      <div className="buttons">
+        <div className="operators">
+          <div onClick={() => updateResult('+')}>+</div>
+          <div onClick={() => updateResult('-')}>-</div>
+          <div onClick={() => updateResult('*')}>*</div>
+          <div onClick={() => updateResult('/')}>/</div>
+        </div>
+        <div className="leftPanel">
+          <div className="numbers">{createDigits(7, 9)}</div>
+          <div className="numbers">{createDigits(4, 6)}</div>
+          <div className="numbers">{createDigits(1, 3)}</div>
+          <div className="numbers">
+            <div onClick={() => updateResult('0')}>0</div>
+            <div onClick={() => updateResult('.')}>.</div>
+            <div onClick={reset}>AC</div>
+          </div>
+        </div>
+        <div className="equal" onClick={Resultulate}>
+          =
+        </div>
+      </div>
     </div>
   );
 }
